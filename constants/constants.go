b807ce8c7efe8e9bb42cdf25ac3fa0e72157db07
Marwan Hawari <marwan.hawari@gmail.com>
2022-02-23 22:54:07 -0800

    fix regex escape characters

--- constants/constants.go
+++ constants/constants.go
@@ -38,10 +38,10 @@ var RegexAmd64 = `(?i)(x86_64|amd64|x64)`
 var Regex386 = `(?i)(i?386|x86_32|amd32|x32)`
 
 // RegexGithub is a regular express for valid GitHub repos
-var RegexGithub = `(?i)^[A-Za-z0-9-]+\/[A-Za-z0-9_.-]+(@.+)?$`
+var RegexGithub = `(?i)^[A-Za-z0-9\-]+\/[A-Za-z0-9\_\.\-]+(@.+)?$`
 
 // RegexGithubSearch is a regular express for valid GitHub search queries
-var RegexGithubSearch = `(?i)^[A-Za-z0-9_.-/]+$`
+var RegexGithubSearch = `(?i)^[/A-Za-z0-9\_\.\-]+$`
 
 // RegexURL is a regular express for valid URLs
 var RegexURL = `(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])`
