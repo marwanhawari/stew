16d6fdb6e700db018604a8d7d93cd5f356822515
Marwan Hawari <marwan.hawari@gmail.com>
2022-02-23 23:13:57 -0800

    allow forward slash in github search query

--- constants/constants.go
+++ constants/constants.go
@@ -41,7 +41,7 @@ var Regex386 = `(?i)(i?386|x86_32|amd32|x32)`
 var RegexGithub = `(?i)^[A-Za-z0-9\-]+\/[A-Za-z0-9\_\.\-]+(@.+)?$`
 
 // RegexGithubSearch is a regular express for valid GitHub search queries
-var RegexGithubSearch = `(?i)^[/A-Za-z0-9\_\.\-]+$`
+var RegexGithubSearch = `(?i)^[A-Za-z0-9\_\.\-\/]+$`
 
 // RegexURL is a regular express for valid URLs
 var RegexURL = `(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])`
